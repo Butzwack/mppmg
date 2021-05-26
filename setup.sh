25077a980330b3af3eeb139e0e29d9a6352fdeb5
Butzwack <83119138+Butzwack@users.noreply.github.com>
2021-05-26 22:32:37 +0200

    Added .config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cp .config ~/
 cat .bashrc >> ~/.bashrc
 cp {.bash_aliases,.vimrc} ~/
-sudo apt install vim ffmpeg build-essential python3-pip python-is-python3 vpx-tools aom-tools htop cmake gettext python3-opencv kdenlive mesa-utils libsm6 libcurl4-gnutls-dev libsdl2-mixer-dev libsdl2-dev libpango1.0-dev libcairo2-dev libavcodec-dev libavresample-dev libglew-dev librtmp-dev libjpeg-dev libavformat-dev liblzma-dev neofetch scons pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm nasm
 mkdir git bin
 cp qencoder bin
+sudo apt install vim ffmpeg build-essential python3-pip python-is-python3 vpx-tools aom-tools htop cmake gettext python3-opencv kdenlive mesa-utils libsm6 libcurl4-gnutls-dev libsdl2-mixer-dev libsdl2-dev libpango1.0-dev libcairo2-dev libavcodec-dev libavresample-dev libglew-dev librtmp-dev libjpeg-dev libavformat-dev liblzma-dev neofetch scons pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm nasm
