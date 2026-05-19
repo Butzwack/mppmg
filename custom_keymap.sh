1fc993e8ea6a73fc63a3c76ad9cccc4112278343
Butzwack <83119138+Butzwack@users.noreply.github.com>
2026-05-19 05:14:43 +0200

    remove deprecated files

--- custom_keymap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xmodmap -e "keycode 29 = y"
-xmodmap -e "keycode 52 = z"
-xmodmap -e "keycode 49 = asciicircum"
-
--- package/test/install/youtube-dl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://yt-dl.org/downloads/latest/youtube-dl -O ~/bin/youtube-dl
-chmod a+rx ~/bin/youtube-dl
